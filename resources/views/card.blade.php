ee4bbf4dc88fd8c3c5118e79b6b0b26087b0c311
simone ricciardo <you@example.com>
2022-09-27 17:35:31 +0200

    first commit

---
+++ resources/views/card.blade.php
@@ -0,0 +1,24 @@
+@extends('app')
+
+@section('content')
+
+<div class="cont_style">
+    <div class=" style_jumbo">
+        <img src="" alt="">
+    </div>
+    <div class="my_container mystyle">
+        <div class="">
+            @foreach ($comic as $key => $formato)
+            <div class="">
+                <img class="img_card" src="{{$formato['thumb']}}" :alt="">
+                <div>
+                    <span>{{$formato['title']}}</span>
+                    <span>{{}}</span>
+                </div>
+            </div> 
+            @endforeach 
+        </div>
+    </div>
+</div>
+
+@endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,59 @@
+<footer>
+    <div class="mystyle">
+        <ul class="my_container">
+            <li>
+            </li>
+        </ul>
+    </div>
+    <div>
+        <div class="back_footer_link">
+            <div class="my_container containet_list_style">
+                <div class="style_list">
+                    <div>
+                        <h3>DC COMICS</h3>
+                        <ul>
+                            <li>
+                            </li>
+                        </ul>
+                        <h3>SHOP</h3>
+                        <ul>
+                            
+                        </ul>
+                    </div>
+                    <div>
+                        <h3>DC</h3>
+                        <ul>
+                            
+                        </ul>
+                    </div>
+                    <div>
+                        <h3>SITES</h3>
+                        <ul>
+                            
+                        </ul>
+                    </div>
+                </div>
+                <div class="">
+                    <img src="" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="link_social_bar">
+            <div class="my_container link_social">
+                <div>
+                    <button>SIGN-UP NOW!</button>
+                </div>
+                <div>
+                    <ul class="icon_link">
+                        <h3>Follow us</h3>
+                        <li>
+                            <span class="flex_social">
+                                <a ><img src="" alt=""></a>
+                            </span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
